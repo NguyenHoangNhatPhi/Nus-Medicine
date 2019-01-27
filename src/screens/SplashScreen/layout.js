a31efab0ff8b6215c06971dee7642dbf125ccc29
NhatPhi2201 <nguyenhoangnhatphi@gmail.com>
2019-01-27 15:46:40 +0700

    UI splash screen

--- src/screens/SplashScreen/layout.js
+++ src/screens/SplashScreen/layout.js
@@ -6,20 +6,28 @@ import {
     ImageBackground
 } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
+import Shimmer from 'react-native-shimmer';
 
 import { Button, HeaderScreen, AutoGrowingTextInput } from '../../components';
 import { scaleSzie } from '../../utils/func';
 import styles from './style';
 import Configs from '../../configs';
 
-const BACKGROUND = require('../../resources/background.jpg');
+const BACKGROUND = require('../../resources/background.png');
 
 export default class Layout extends React.Component {
 
     render() {
         return (
             <ImageBackground source={BACKGROUND} style={styles.container}>
-                <ActivityIndicator 
+                <View style={{alignItems:'center',marginBottom:scaleSzie(50)}} >
+                    <Shimmer>
+                        <Text
+                            style={{color:'#fff',fontSize:scaleSzie(35),fontWeight:'bold'}}
+                        >Nus Medicine</Text>
+                    </Shimmer>
+                </View>
+                <ActivityIndicator
                     color="#fff"
                     size="large"
                 />
