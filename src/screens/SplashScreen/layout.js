c57df93ee50049c6f861921ade717e6a42810b5a
NhatPhi2201 <nguyenhoangnhatphi@gmail.com>
2019-01-27 15:40:37 +0700

    setup react-native-shimmer

--- src/screens/SplashScreen/layout.js
+++ src/screens/SplashScreen/layout.js
@@ -2,23 +2,28 @@ import React from 'react';
 import {
     View,
     Text,
-
+    ActivityIndicator,
+    ImageBackground
 } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
-
 
 import { Button, HeaderScreen, AutoGrowingTextInput } from '../../components';
 import { scaleSzie } from '../../utils/func';
 import styles from './style';
 import Configs from '../../configs';
 
-export default class Layout extends React.Component {
+const BACKGROUND = require('../../resources/background.jpg');
 
+export default class Layout extends React.Component {
 
     render() {
         return (
-            <View style={styles.container}>
-            </View>
+            <ImageBackground source={BACKGROUND} style={styles.container}>
+                <ActivityIndicator 
+                    color="#fff"
+                    size="large"
+                />
+            </ImageBackground>
         );
     }
 }
--- src/screens/SplashScreen/style.js
+++ src/screens/SplashScreen/style.js
@@ -12,39 +12,8 @@ const {width,height} = Dimensions.get('window');
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor :'rgb(250,250,250)'
+        justifyContent: 'center',
+        alignContent: 'center',
+        // backgroundColor :'rgb(250,250,250)'
     },
-    textSetting:{
-        color:'rgb(0,127,122)',
-        fontSize:scaleSzie(14),
-        fontWeight :'600',
-        marginLeft:scaleSzie(16),
-        ...Platform.select({
-            ios:{
-                marginTop:scaleSzie(40)
-            },
-            android:{
-                marginTop:scaleSzie(20)
-            }
-        })
-    },
-    containerContent:{
-        height:scaleSzie(160),
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        borderTopColor:'rgb(230,230,230)',
-        borderBottomColor:'rgb(230,230,230)',
-    },
-    line:{
-        height:1,
-        backgroundColor:'rgb(230,230,230)',
-    },
-    textTitle:{
-        color:'rgb(33,33,33)',
-        fontSize:scaleSzie(14),
-    },
-    textDesc:{
-        color:'rgb(147,147,147)',
-        fontSize:scaleSzie(13),
-    }
 })
