013492d8d959c3d6c8a686938f526d7765aa9e34
phinhn <nguyenhoangnhatphi@gmail.com>
2019-02-15 17:24:49 +0700

    setup

--- android/app/src/main/java/com/nus/medicine/MainActivity.java
+++ android/app/src/main/java/com/nus/medicine/MainActivity.java
@@ -1,9 +1,14 @@
 package com.nus.medicine;
 
+import android.os.Bundle;
+import android.content.Intent;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -11,6 +16,12 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript. This is
      * used to schedule rendering of the component.
      */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      Fabric.with(this, new Crashlytics());
+    }
+
     @Override
     protected String getMainComponentName() {
         return "nusmedicine";
