088e86bfdaadfbb321e5ea832fd27f4bca1745ad
phinhn <>
2019-08-17 22:47:17 +0700

    Change endpoint app

--- src/configs/api.js
+++ src/configs/api.js
@@ -4,8 +4,11 @@ const apiConfigs = {
     // BASE_SOCKET: "http://816e95a8.ngrok.io",
 
     // ---- Production -----
-    BASE_API: "http://3.0.93.22:3000/api/v1/",
-    BASE_SOCKET: "http://3.0.93.22:3000"
+    // BASE_API: "http://3.0.93.22:3000/api/v1/",
+    // BASE_SOCKET: "http://3.0.93.22:3000"
+
+    BASE_API: "https://medicinealumni-app.nus.edu.sg/api/v1/",
+     BASE_SOCKET: "https://medicinealumni-app.nus.edu.sg"
 
 
 }
